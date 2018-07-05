accd7ba881fca66ae5371d6642a351fcfca40507
Stanislava Balakhovska <balakhovskastanislava@gmail.com>
2018-07-05 17:47:49 +0300

    Added REST client for web_app

--- rest/app.py
+++ rest/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from os import path
 import json
 
-from rest.data.data import TASK_TO_ADD, USER_TO_ADD
+from rest.data.data import USER_TO_ADD
 
 app = Flask(__name__)
 
@@ -52,10 +52,12 @@ def get_user(user_id):
 
 
 @app.route('/users/<int:user_id>/tasks', methods=['GET'])
-def get_user_tasks(user_id):
+def get_user_tasks(user_id, start='', end=''):
+    # ToDo(stasya): add request to database
     user_tasks = list()
     for task in tasks:
         if task['user_id'] == user_id:
+            # SQL request for the given period
             user_tasks.append(task)
 
     return json.dumps(user_tasks)
@@ -70,9 +72,9 @@ def get_task(task_id):
 
 
 @app.route('/tasks', methods=['POST'])
-def add_task():
-    tasks.append(TASK_TO_ADD)
-    return json.dumps(tasks)
+def add_task(task):
+    tasks.append(task)
+    return render_template('index.html'), 201
 
 
 @app.route('/tasks/<int:task_id>', methods=['DELETE'])
@@ -80,7 +82,20 @@ def delete_task(task_id):
     for task in tasks:
         if task['id'] == task_id:
             tasks.remove(task)
-            return json.dumps(users)
+            return json.dumps(tasks)
+    return not_found(404)
+
+
+@app.route('/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id, **kwargs):
+    # ToDo(stasya) add logic for updating values for existing keys
+    # add check whether key is in kwargs
+    for task in tasks:
+        if task['id'] == task_id:
+            for item in kwargs:
+                item = dict([item])
+                task.update(item)
+            return json.dumps(task)
     return not_found(404)
 
 
