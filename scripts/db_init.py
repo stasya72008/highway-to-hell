a9ddf9071a0e3c6fb4bee3d5da217e2db290d81a
Stanislava Balakhovska <balakhovskastanislava@gmail.com>
2018-08-03 12:36:56 +0300

    Fixes for checks in scripts

--- scripts/db_init.py
+++ scripts/db_init.py
@@ -15,24 +15,24 @@ cursor.execute(query)
 
 query = "show tables"
 cursor.execute(query)
-if cursor.rowcount == 0:
-    query = 'CREATE table users (' \
-            'ID int AUTO_INCREMENT, ' \
-            'Username varchar(255) NOT NULL,' \
-            'PRIMARY KEY (ID)' \
-            ')'
-    cursor.execute(query)
 
-    query = "CREATE table tasks (" \
-            "ID int AUTO_INCREMENT, " \
-            "UserID int NOT NULL," \
-            "Name varchar(255) NOT NULL," \
-            "Date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()," \
-            "Status enum('active', 'done', 'deleted', 'archive') NOT NULL DEFAULT 'active'," \
-            "Calendar_date varchar(15)," \
-            "PRIMARY KEY (ID)," \
-            "FOREIGN KEY (UserID) REFERENCES users(ID)" \
-            ")"
-    cursor.execute(query)
+query = 'CREATE table IF NOT EXISTS users (' \
+        'ID int AUTO_INCREMENT, ' \
+        'Username varchar(255) NOT NULL,' \
+        'PRIMARY KEY (ID)' \
+        ')'
+cursor.execute(query)
+
+query = "CREATE table IF NOT EXISTS tasks (" \
+        "ID int AUTO_INCREMENT, " \
+        "UserID int NOT NULL," \
+        "Name varchar(255) NOT NULL," \
+        "Date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP()," \
+        "Status enum('active', 'done', 'deleted', 'archive') NOT NULL DEFAULT 'active'," \
+        "Calendar_date varchar(15)," \
+        "PRIMARY KEY (ID)," \
+        "FOREIGN KEY (UserID) REFERENCES users(ID)" \
+        ")"
+cursor.execute(query)
 
 db.close()
--- scripts/db_v2.py
+++ scripts/db_v2.py
@@ -7,23 +7,28 @@ db = sql.connect(host=conf.host, user=conf.user, port=int(conf.port),
                  passwd=conf.password)
 
 cursor = db.cursor()
-query = "USE highway"
-cursor.execute(query)
 
-query = "SELECT column_name FROM information_schema.columns WHERE table_name = 'users'"
-cursor.execute(query)
-if cursor.rowcount == 2:
-    query = "CREATE table roles(" \
-            "ID int AUTO_INCREMENT," \
-            "Role varchar(50) NOT NULL," \
-            "PRIMARY KEY (ID)" \
-            ")"
-    cursor.execute(query)
-    query = "INSERT INTO roles (ID, Role) VALUES (1, Admin), (2, Regular)"
+try:
+    cursor.execute("USE highway")
+    cursor.execute("SHOW TABLES")
 
-    query = "ALTER TABLE users ADD Role int DEFAULT 2"
-    cursor.execute(query)
-    query = "ALTER TABLE tasks ADD Position int DEFAULT 999999"
-    cursor.execute(query)
+    if not any([t for t in cursor.fetchall() if t[0] == 'roles']):
+        query = "CREATE table roles(" \
+                "ID int AUTO_INCREMENT," \
+                "Role varchar(50) NOT NULL," \
+                "PRIMARY KEY (ID)" \
+                ")"
+        cursor.execute(query)
+        query = "INSERT INTO roles (ID, Role) VALUES (1, 'Admin')"
+        db.cursor().execute(query)
+        query = "INSERT INTO roles (ID, Role) VALUES (2, 'Regular')"
+        db.cursor().execute(query)
+        query = "ALTER TABLE users ADD Role int DEFAULT 2"
+        db.cursor().execute(query)
+        query = "ALTER TABLE users ADD FOREIGN KEY (Role) REFERENCES roles(ID)"
+        db.cursor().execute(query)
+        query = "ALTER TABLE tasks ADD Position int DEFAULT 999999"
+        db.cursor().execute(query)
 
-db.close()
+finally:
+    db.close()
